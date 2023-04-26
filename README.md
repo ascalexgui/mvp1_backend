c5287cd23f8ef2a7fd265738aeace2d1c3d5d617
ascalexgui <89161930+ascalexgui@users.noreply.github.com>
2023-04-26 18:16:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# mvp1_backend
-Backend do MVP 1 do curso "Desenvolvimento Full Stack"
+# Minha API
+
+Este pequeno projeto faz parte do primeiro MVP da Sprint I do curso **Desenvolvimento Full Stack Básico** 
+
+O objetivo aqui é apresentar o conhecimento adquirido ao longo do módulo 01 que englobou os seguinte módulos:
+
+1) Programação Orientada a Objetos
+2) Banco de dados
+3) Desenvolvimento Full Stack Básico
+
+O banco é carregado na __init__.py
+
+Uso de um recurso ORM facilita o mapeamento entre aquilo que está no banco e as classes de negócio.
+
+A BASE 
+
+---
+## Como executar 
+
+
+Será necessário ter todas as libs python listadas no `requirements.txt` instaladas.
+Após clonar o repositório, é necessário ir ao diretório raiz, pelo terminal, para poder executar os comandos descritos abaixo.
+
+> É fortemente indicado o uso de ambientes virtuais do tipo [virtualenv](https://virtualenv.pypa.io/en/latest/installation.html).
+
+```
+(env)$ pip install -r requirements.txt
+```
+
+Este comando instala as dependências/bibliotecas, descritas no arquivo `requirements.txt`.
+
+Para executar a API  basta executar:
+
+```
+(env)$ flask run --host 0.0.0.0 --port 5000
+```
+
+Em modo de desenvolvimento é recomendado executar utilizando o parâmetro reload, que reiniciará o servidor
+automaticamente após uma mudança no código fonte. 
+
+```
+(env)$ flask run --host 0.0.0.0 --port 5000 --reload
+```
+
+Abra o [http://localhost:5000/#/](http://localhost:5000/#/) no navegador para verificar o status da API em execução.
---
+++ app.py
@@ -0,0 +1,148 @@
+from flask_openapi3 import OpenAPI, Info, Tag
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import func
+from flask_cors import CORS
+from flask import redirect
+
+from model import Session, Despesa
+from logger import logger
+from schemas import *
+
+info = Info(title="API Fecha a Conta", version="1.0.0")
+app = OpenAPI(__name__, info=info)
+CORS(app)
+
+
+# definindo tags
+home_tag = Tag(name="Documentação", description="Documentação: Swagger")
+despesa_tag = Tag(name="Despesa", description="Adição, visualização e remoção das despesas à base")
+
+
+@app.get('/', tags=[home_tag])
+def home():
+    """Redireciona para o SWAGGER com a documentação.
+    """
+    return redirect('/openapi/swagger')
+
+"""
+------------------------------------------
+Adiciona uma nova despesa a base de dados 
+& 
+Retorna uma representação das despesas. 
+POST
+------------------------------------------
+"""
+
+@app.post('/inclui_despesa', tags=[despesa_tag],
+          responses={"200": DespesaViewSchema, "409": ErrorSchema, "400": ErrorSchema})
+def inclui_despesa(form: DespesaSchema):
+    """Adiciona uma nova despesa informando o nome, quantidade e valor"""
+
+    despesa = Despesa(
+        nome=form.nome,
+        quantidade=form.quantidade,
+        valor=form.valor)
+
+    try:
+        # criando conexão com a base
+        session = Session()
+        # adicionando despesa
+        session.add(despesa)
+        # efetivando o camando de adição da nova despesa na tabela
+        session.commit()
+        return apresenta_despesa(despesa), 200
+
+    except Exception as e:
+        # caso um erro fora do previsto
+        error_msg = "Não foi possível salvar nova despesa :/"
+        return {"message": error_msg}, 400
+
+"""
+------------------------------------------
+Faz a busca por todas as Despesas cadastradas 
+& 
+Retorna uma representação da listagem das despesas e total das despesas ( soma quantidade * valor de cada despesa)
+GET
+------------------------------------------
+"""
+
+@app.get('/lista_despesas', tags=[despesa_tag],
+         responses={"200": ListagemDespesasSchema, "404": ErrorSchema})
+def get_lista_despesas():
+    """Retorna a lista com as despesas já cadastradas"""
+
+    # criando conexão com a base
+    session = Session()
+    # fazendo a busca
+    despesas = session.query(Despesa).all()
+
+    # despesas = session.query(Despesa).order_by(Despesa.nome.asc()).all
+
+    if not despesas:
+        # se não há despesas cadastradas
+        return {"despesas":[], "total_gasto":0}, 200
+    else:
+        # retorna a representação das despesas
+        return apresenta_despesas(despesas), 200
+
+"""
+------------------------------------------------
+Busca por uma Despesa a partir do nome da despesa
+&
+Retorna uma representação das despesas.
+GET
+------------------------------------------------
+"""
+
+@app.get('/despesa_por_nome', tags=[despesa_tag],
+         responses={"200": DespesaViewSchema, "404": ErrorSchema})
+def get_despesa_por_nome(query: DespesaBuscaNomeSchema):
+    """Busca por uma despesa já cadastrada através do seu nome"""
+
+    nome = query.nome.lower()
+    # criando conexão com a base
+    session = Session()
+    # fazendo a busca
+    despesa = session.query(Despesa).filter(func.lower(Despesa.nome) == nome).first()
+
+    if not despesa:
+        # se o despesa não foi encontrada
+        error_msg = "Despesa não encontrada na base :/"
+        logger.warning(f"Erro ao buscar produto ' {nome}, {error_msg}")
+        return {"message": error_msg}, 404
+    else:
+        # retorna a representação da despesa
+        return apresenta_despesa(despesa), 200
+
+
+"""
+------------------------------------------------
+Deleta uma Despesa a partir do seu nome
+&
+Retorna uma mensagem de confirmação da remoção.
+DELETE
+------------------------------------------------
+"""
+
+@app.delete('/deleta_por_nome', tags=[despesa_tag],
+            responses={"200": DespesaDelSchema, "404": ErrorSchema})
+def del_deleta_por_nome(query: DespesaBuscaNomeSchema):
+    """Deleta uma despesa a partir do seu nome"""
+
+    nome = query.nome
+
+    # criando conexão com a base
+    session = Session()
+    # fazendo a remoção
+    despesa = session.query(Despesa).filter(Despesa.nome == nome).first()
+   
+    
+    if despesa:
+        session.query(Despesa).filter(Despesa.nome == nome).delete()
+        session.commit()
+        # retorna a representação da mensagem de confirmação        
+        return {"message": "Despesa removida", "nome": nome}
+    else:
+        # se o despesa não foi encontrada
+        error_msg = "Despesa não encontrado na base :/"
+        return {"message": error_msg}, 404
